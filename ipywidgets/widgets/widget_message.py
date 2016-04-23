3d1566b31fc30e54127490371c46ada41380912b
Dave Willmer <dave.willmer@gmail.com>
2016-04-23 14:46:13 +0100

    Move context manager to ipykernel

--- ipywidgets/widgets/widget_message.py
+++ ipywidgets/widgets/widget_message.py
@@ -8,14 +8,13 @@ Represents a widget that can be used to intercept display messages.
 # Distributed under the terms of the Modified BSD License.
 
 from .domwidget import DOMWidget
-import sys
-from io import StringIO
 from traitlets import (
-    Unicode, List, Instance, default, observe, Any
+    Unicode, List, Instance, default, observe
 )
 from IPython.display import clear_output
 from IPython import get_ipython
 from ipykernel.displayhook import ZMQMessageHook
+from ipykernel.hookmanager import MessageHookFor
 from ipykernel.zmqshell import ZMQDisplayPublisher
 from jupyter_client.session import Message
 
@@ -32,10 +31,10 @@ class MessageWidget(DOMWidget):
     -------
     >>> import ipywidgets as widgets
     >>> from IPython.display import display
-    >>> hooked = widgets.MessageWidget('display_data')
-    >>> display(hooked)
+    >>> my_widget = widgets.MessageWidget('display_data')
+    >>> display(my_widget)
     >>> print('prints to output area')
-    >>> with hooked:
+    >>> with my_widget.capture():
             display('prints to message widget')
     """
     _view_name = Unicode('MessageWidgetView').tag(sync=True)
@@ -47,71 +46,25 @@ class MessageWidget(DOMWidget):
     # messages, change this attribute name.
     _message_type = Unicode('display_data')
 
-    _message_hook = Instance(ZMQMessageHook)
-
-    # Hookable messages are only implemented on the ZMQDisplayPublisher
-    #
-    _pub = Instance(ZMQDisplayPublisher)
-
     # The list of messages stored by the hook when activated
     # (in context).
     #
     stored_messages = List().tag(sync=True)
 
+    # The context manager for capturing the messages.
     #
-    # String buffer to catch stdout / stderr
-    #
-    _std_buffer = Any()
-
-    @default('_message_hook')
-    def _message_hook_default(self):
-        return ZMQMessageHook(self._message_type, self.store)
-
-    @default('_pub')
-    def _pub_default(self):
-        return get_ipython().display_pub
+    capture = Instance(MessageHookFor)
 
     @default('_std_buffer')
     def _std_buffer_default(self):
         return StringIO()
 
     def clear_output(self, *args, **kwargs):
-        with self:
-            self.clear()
+        self.clear()
 
-    def __enter__(self):
-        """
-        Called when entering MessageWidget context manager
-        """
-        self._pub.register_hook(self._message_hook)
-        self._old_clear = self._pub.clear_output
-        self._pub.clear_output = self.clear_output
-
-        self._old_stdout = sys.stdout
-        self._old_stderr = sys.stderr
-        sys.stdout = self._std_buffer
-        sys.stderr = self._std_buffer
-        return self
-
-    def __exit__(self, tp, value, tb):
-        """
-        Called when exiting MessageWidget context manager.
-        """
-        if tp is not None:
-            # TODO : Exception occurred... log and continue.
-            pass
-
-        self._pub.unregister_hook(self._message_hook)
-        self._pub.clear_output = self._old_clear
-        sys.stdout = self._old_stdout
-        sys.stderr = self._old_stderr
-
-        # TODO : remove this when rendermime working in frontend.
-        std = self._std_buffer.getvalue()
-        if std:
-            temp = {'content': {'data': {'text/plain': self._std_buffer.getvalue()}}}
-            self.store(temp)
-            self._std_buffer.truncate(0)
+    def capture(self, message_type=None):
+        msg_type = message_type or self._message_type
+        return MessageHookFor(msg_type, parent=self)
 
     def clear(self):
         """
